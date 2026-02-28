54cc65a869df0b4620c768924173c21ac41e33ab
zacsi33 <nzalan33@gmail.com>
2026-02-28 20:19:42 +0100

    Hibak kijavitasa es mukodo kod

--- main.cpp
+++ main.cpp
@@ -4,40 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // 1. NELEMENTS hibás volt, N_ELEMENTS a helyes konstans
-    int *b = new int[N_ELEMENTS]; 
-    
-    // 2. Kettős idézőjel kell a stringekhez, és hiányzott a pontosvessző a végéről
-    std::cout << "1-100 ertekek duplazasa" << std::endl; 
-    
-    // 3. A for ciklus hiányos volt (hiányzott a feltétel és az inkrementálás)
-    for (int i = 0; i < N_ELEMENTS; i++) 
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = (i + 1) * 2; // (Opcionális) 1-100 közötti értékek duplázása
+        b[i] = i * 2;
     }
-    
-    // 4. A ciklusfeltétel (i) nem volt megfelelő, és hiányzott a kiíratás vége
     for (int i = 0; i < N_ELEMENTS; i++)
     {
         std::cout << "Ertek: " << b[i] << std::endl;
     }    
-    
     std::cout << "Atlag szamitasa: " << std::endl;
-    
-    // 5. Az átlagot inicializálni kell 0-ra, és double célszerűbb a precizitáshoz
-    double atlag = 0; 
-    
-    // 6. A for ciklusban vessző helyett pontosvessző kell az elválasztáshoz
+    int atlag = 0;
     for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]; // Hiányzó pontosvessző pótolva
+        atlag += b[i];
     }
-    
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-
-    // 7. Memória felszabadítása (jó gyakorlat)
-    delete[] b;
 
     return 0;
 }
